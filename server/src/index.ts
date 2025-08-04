d49ab89afbba40c141e0a9d33333c0c6094ff8fb
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-08-04 00:52:00 +0000

    chore: move entry point of index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
-import app from "./src/app";
+import app from "./app";
 import dotenv from "dotenv";
-import connectDB from "./src/config/db";
+import connectDB from "./config/db";
 
 dotenv.config();
 
